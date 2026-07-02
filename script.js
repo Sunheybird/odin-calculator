a86a71f23d4c476d0d4e0f6058eb2784f0eebc1e
Elvis <1155260126@link.cuhk.edu.hk>
2026-07-02 18:25:51 +0800

    finished the function of top symbols

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ let a = 0;
 let operator = null;
 let b = null;
 
+function reset(){
+    a = 0;
+    operator = null;
+    b = null;
+}
+
 for(let i = 0;i < buttonValues.length;i++){
     let value = buttonValues[i]; //string
     let button = document.createElement('button');
@@ -48,12 +54,24 @@ for(let i = 0;i < buttonValues.length;i++){
             }
         }
 
+        if(topSymbols.include(value)){
+            if(value == 'AC'){
+                display.value = ''; //reset the look
+                reset(); //reset the function
+            }
+            if(value == '+/-'){
+                if(display.value != '' && display.value != '0'){
+                    display.value = Number(display.value)*(-1);
+                }
+            }
+            if(value == '%'){
+                display.value = Number(display.value)/100; //need to convert into number
+            }
+        }
+
         if(rightSymbols.include(value)){
 
         }
 
-        if(topSymbols.include(value)){
-
-        }
     })
 }
