120447add694b847041f790279962094443599cc
Elvis <1155260126@link.cuhk.edu.hk>
2026-07-02 18:12:10 +0800

    finished the display of number and dot

---
+++ script.js
@@ -0,0 +1,59 @@
+const buttonValues = [
+    'AC','+/-','%','÷',
+    '7','8','9','×',
+    '4','5','6','-',
+    '1','2','3','+',
+    '0','.','='
+];
+
+const rightSymbols = [
+    '÷','×','-','+','='
+];
+
+const topSymbols = [
+    'AC','+/-','%'
+];
+
+const numbers = [
+    '7','8','9',
+    '4','5','6',
+    '1','2','3',
+    '0','.'
+];
+
+const display = document.querySelector('.display');
+
+let a = 0;
+let operator = null;
+let b = null;
+
+for(let i = 0;i < buttonValues.length;i++){
+    let value = buttonValues[i]; //string
+    let button = document.createElement('button');
+    button.innerText = value;
+    button.classList = value;
+
+    button.addEventListener('click',()=>{
+        if(numbers.include(value)){
+            if(value == '.'){
+                if(display.value != '' && display.value.include('.')){ //avoid multiple '.'
+                    display.value += value;
+                }
+            }
+            else if(display.value == '0'){
+                display.value = value; //this is hard, avoid multiple 0. It means 0 = reset at first.
+            }
+            else{ //numbers
+                display.value += value;
+            }
+        }
+
+        if(rightSymbols.include(value)){
+
+        }
+
+        if(topSymbols.include(value)){
+
+        }
+    })
+}
